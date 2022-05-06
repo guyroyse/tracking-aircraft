3df9497e0fd44289e9da6595c4d9898f5db3a419
Guy Royse <guy@guyroyse.com>
2022-05-06 15:24:17 -0400

    a tidied ingestor

--- radio-ingestor/server.js
+++ radio-ingestor/server.js
@@ -2,18 +2,81 @@ import 'dotenv/config'
 import * as sbs1 from 'sbs1'
 import * as redis from 'redis'
 
-const host = process.env['DUMP_1090_HOST']
-const port = Number(process.env['DUMP_1090_PORT'])
+const sbs1Host = process.env['SBS1_SOURCE_HOST'] ?? 'localhost'
+const sbs1Port = Number(process.env['SBS1_SOURCE_PORT'] ?? 30003)
+const redisHost = process.env['REDIS_HOST'] ?? 'localhost'
+const redisPort = Number(process.env['REDIS_PORT'] ?? 6379)
+const redisPassword = process.env['REDIS_PASSWORD']
+
 const streamKey = process.env['STREAM_KEY']
 
-const sbs1Client = sbs1.createClient({ host, port })
-const redisClient = redis.createClient()
+const sbs1Client = sbs1.createClient({ host: sbs1Host, port: sbs1Port })
+const redisClient = redis.createClient({
+  socket: { host: redisHost, port: redisPort },
+  password: redisPassword })
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err))
 await redisClient.connect()
 
+const transmissionTypes = [
+  'ES_IDENT_AND_CATEGORY',
+  'ES_SURFACE_POS',
+  'ES_AIRBORNE_POS',
+  'ES_AIRBORNE_VEL',
+  'SURVEILLANCE_ALT',
+  'SURVEILLANCE_ID',
+  'AIR_TO_AIR',
+  'ALL_CALL_REPLY'
+]
+
 sbs1Client.on('message', msg => {
-  const aircraftId = msg.hex_ident
+
+  let event = {}
+
+  event.icacoId = msg.hex_ident
+  event.type = transmissionTypes[msg.transmission_type - 1]
+  event.generatedDateTime = toEpochMilliseconds(msg.generated_date, msg.generated_time)
+  event.loggedDateTime = toEpochMilliseconds(msg.logged_date, msg.logged_time)
+
+  if (msg.transmission_type === 1) {
+    event.callsign = msg.callsign.trim()
+    event.onGround = Boolean(msg.is_on_ground)
+  }
+
+  if (msg.transmission_type === 2) {
+    console.log(msg)
+  }
+
+  if (msg.transmission_type === 3) {
+    event.altitude = msg.altitude
+    event.latitude = msg.lat
+    event.longitude = msg.lon
+    event.onGround = Boolean(msg.is_on_ground)
+  }
+
+  if (msg.transmission_type === 4) {
+    event.velcoity = msg.ground_speed
+    event.heading = msg.track
+    event.climb = msg.vertical_rate
+    event.onGround = Boolean(msg.is_on_ground)
+  }
+
+  if (msg.transmission_type === 5) {
+    event.altitude = msg.altitude
+  }
+
+  if (msg.transmission_type === 6) {
+    console.log(msg)
+  }
+
+  if (msg.transmission_type === 7) {
+    event.altitude = msg.altitude
+  }
+
+  if (msg.transmission_type === 8) {
+    event.onGround = Boolean(msg.is_on_ground)
+  }
+
   const minId = new Date().getTime() - 30 * 60 * 1000
   const trimOptions = {
     strategy: 'MINID',
@@ -21,7 +84,9 @@ sbs1Client.on('message', msg => {
     threshold: minId
   }
 
-  redisClient.xAdd(streamKey, '*', msg, { TRIM: trimOptions })
-  redisClient.xAdd(`${streamKey}:${aircraftId}`, '*', msg, { TRIM: trimOptions })
-  redisClient.sAdd(`${streamKey}:aircraft`, aircraftId)
+  redisClient.xAdd(streamKey, '*', event, { TRIM: trimOptions })
 })
+
+function toEpochMilliseconds(dateString, timeString) {
+  return new Date(`${dateString.replaceAll('/', '-')}T${timeString}`).getTime()
+}
