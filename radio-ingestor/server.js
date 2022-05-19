5f76f2bea461e076275db21a3d2cc2d3d34e57f4
Guy Royse <guy@guyroyse.com>
2022-05-19 12:58:04 -0400

    removed logging

--- radio-ingestor/server.js
+++ radio-ingestor/server.js
@@ -71,8 +71,6 @@ sbs1Client.on('message', msg => {
   const thirtyMinutes = 30 * 60 * 1000
   const minId = new Date().getTime() - thirtyMinutes
 
-  console.log(event.loggedDateTime)
-
   redisClient.xAdd(
     streamKey, '*', event, {
       TRIM: {
