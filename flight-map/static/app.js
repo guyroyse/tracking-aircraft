ee4d567339087015c0a6f28c7ab3e2a74999ce18
Guy Royse <guy@guyroyse.com>
2022-05-26 13:58:11 -0400

    tidy code, tidy icons, tidy headings

--- flight-map/static/app.js
+++ flight-map/static/app.js
@@ -1,59 +1,63 @@
-// All JS Goes here -Guy Royse
+// map and icons
+const map = L.map('map')
+const homeIcon = L.icon({ iconUrl: 'icons/home.png', iconSize: [16, 16] })
+const planeIcons = [...Array(24).keys()]
+  .map(n => L.icon({ iconUrl: `icons/plane-${n * 15}.png`, iconSize: [18, 18] }))
 
+// all the marks for aircraft we've spotted so far
+const aircraftMarkers = {}
+
+// add maps to the map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiZ3V5cm95c2UiLCJhIjoiY2wyZXhjdXNkMDQ3NjNra2ptajhlN3J3OSJ9.y3JcnAPeRMLCLifILS8t0Q'
+}).addTo(map);
+
+// add our current location and center the view there
+navigator.geolocation.getCurrentPosition(position => {
+  const { latitude, longitude } = position.coords
+  map.setView([ latitude, longitude ], 8)
+  L.marker([ latitude, longitude ], { icon: homeIcon }).addTo(map)
+})
+
+// set up our event source and handle the events
 const eventSource = new EventSource('http://localhost:8080/events/aircraft/all')
 
-const mechanical_birds = {}
+eventSource.onmessage = event => {
 
-eventSource.onmessage = e => {
-  const message = JSON.parse(e.data)
-  const aircraft = mechanical_birds[message.icacoId] ?? {}
-  mechanical_birds[message.icacoId] = aircraft
+  // parse data out of the event
+  const { icacoId, latitude, longitude, heading } = JSON.parse(event.data)
 
-  aircraft.icacoId = message.icacoId
-  if (message.callsign) aircraft.callsign = message.callsign.trim()
-  if (message.altitude) aircraft.altitude = Number(message.altitude)
-  if (message.velcoity) aircraft.velcoity = Number(message.velcoity)
-  if (message.heading) aircraft.heading = Number(message.heading)
-  if (message.latitude && message.longitude) {
-    aircraft.latlon = { latitude: Number(message.latitude), longitude: Number(message.longitude) }
+  // find our marker
+  let marker = aircraftMarkers[icacoId]
+
+  // create it if it's not found and it has data worthy of creation
+  if (!marker && latitude !== undefined && longitude !== undefined) {
+    marker = L.marker([ latitude, longitude ], { icon: planeIcons[0] }).addTo(map)
+    aircraftMarkers[icacoId] = marker
   }
 
-  console.log(message)
-
-  if (aircraft.latlon) {
-    setMarker(aircraft)
+  // move the marker (if we have one)
+  if (marker) {
+    setMarkerLocation(marker, latitude, longitude)
+    setMarkerHeading(marker, heading)
   }
-
 }
 
-function setMarker(aircraft) {
-    if (!aircraft.marker) {
-        const myDiv = document.createElement('div')
-        document.getElementById('icon-holder').appendChild(myDiv)
-        myDiv.className = `${aircraft.icacoId} plane-icon`
-
-        const planeIcon = L.divIcon({html: myDiv})
-
-        aircraft.icon = myDiv
-        aircraft.marker = L.marker([0,0], { icon: planeIcon }).addTo(map)
-    }
-
-    // Set rotation (if we have data)
-    if (aircraft.heading) {
-      aircraft.icon.style = `transform: rotate(${aircraft.heading}deg);`
-    }
-
-    aircraft.marker.setLatLng([aircraft.latlon.latitude, aircraft.latlon.longitude])
+function setMarkerLocation(marker, latitude, longitude) {
+  if (latitude !== undefined && longitude !== undefined) {
+    marker.setLatLng([latitude, longitude])
+  }
 }
 
-// const map = L.map('map').setView([39.962222, -83.000556], 8)
-const map = L.map('map').setView([40.2757599,-82.2492776], 8)
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZ3V5cm95c2UiLCJhIjoiY2wyZXhjdXNkMDQ3NjNra2ptajhlN3J3OSJ9.y3JcnAPeRMLCLifILS8t0Q'
-}).addTo(map);
+function setMarkerHeading(marker, heading) {
+  if (heading) {
+    let index = Math.round(heading / 15)
+    index = index === 24 ? 0 : index
+    marker.setIcon(planeIcons[index])
+  }
+}
