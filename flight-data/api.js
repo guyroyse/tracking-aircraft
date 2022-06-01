de608d6f77373b27a5cb9d3335df7ad1f1109dee
Guy Royse <guy@guyroyse.com>
2022-06-01 15:50:47 -0400

    various searches

--- flight-data/api.js
+++ flight-data/api.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 /* import routers */
 import { router as flightRouter } from './routers/flight-router.js'
@@ -6,9 +7,15 @@ import { router as searchRouter } from './routers/search-router.js'
 
 export function startServer() {
 
-  /* create an express app and use JSON */
+  /* create an express app and set it up to be excessively permissive */
   const app = new express()
   app.use(express.json())
+  app.use(cors())
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 
   /* bring in some routers */
   app.use('/flight', flightRouter)
