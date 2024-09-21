13cdb72b36ee1679490fd9a2bc6dc3781c83aa78
Guy Royse <guy@guyroyse.com>
2024-09-21 10:50:06 -0400

    removed errantly checked in dist folder

--- radio-ingestor/dist/_config.d.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-export declare const SBS1_HOST: string;
-export declare const SBS1_PORT: number;
-export declare const REDIS_HOST: string;
-export declare const REDIS_PORT: number;
-export declare const REDIS_PASSWORD: string | undefined;
-export declare const AIRCRAFT_STREAM_KEY: string;
-export declare const AIRCRAFT_STREAM_LIEFTIME: number;
-export declare const RADIO_ID: string;
--- radio-ingestor/dist/config.d.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-export declare const sbs1Host: string;
-export declare const sbs1Port: number;
-export declare const redisHost: string;
-export declare const redisPort: number;
-export declare const redisPassword: string | undefined;
-export declare const streamKey: string;
-export declare const streamLifetime: number;
-export declare const radioId: string;
