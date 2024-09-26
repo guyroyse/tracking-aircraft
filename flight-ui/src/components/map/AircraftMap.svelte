f2bed5dc0cad5480aac98f5d7960e51edf4ae805
Guy Royse <guy@guyroyse.com>
2024-09-25 20:15:57 -0400

    implented diffing on aircraft updates seems to ahve fixed performance issue

--- flight-ui/src/components/map/AircraftMap.svelte
+++ flight-ui/src/components/map/AircraftMap.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import Optional from '@guyroyse/optional'
+
   import { homePositionStore } from '../../stores/location-store'
   import { aircraftStore } from '../../stores/aircraft-store'
+  import type { AircraftStatuses } from '../../stores/aircraft-store'
 
   import { AircraftMap } from './aircraft-map'
   import type { AircraftStatusData } from '../../common/aircraft-status'
@@ -8,6 +11,9 @@
   let aircraftMap: AircraftMap
 
   function createMap(element: HTMLElement) {
+    /* Keep track of the previous aircraft statuses. */
+    let previousAircraftStatuses: AircraftStatuses = {}
+
     /* Create the map and bind it to the element. */
     aircraftMap = AircraftMap.create(element)
 
@@ -23,24 +29,50 @@
 
     /* Subscribe to the aircraft store and update the planes on the map. */
     const aircraftUnsubscribe = aircraftStore.subscribe(aircraftStatuses => {
-      console.log('Aircraft status count', Object.keys(aircraftStatuses).length)
-      /* Remove planes that are no longer in the store. */
-      for (const plane of aircraftMap.fetchPlanes()) {
-        const status = aircraftStatuses[plane.icaoId]
-        if (!status) aircraftMap.removePlane(plane.icaoId)
-      }
+      let previousStatus: Optional<AircraftStatusData>
+      let currentStatus: Optional<AircraftStatusData>
 
-      /*
-        TODO: This is the problem. We need to only update aircraft
-        that have changed. This might need to be done in the aircraft
-        store somehow.
-      */
+      console.log('Received aircraft status reports', Object.keys(aircraftStatuses).length)
 
-      /* Add or update planes that *are* in the store. */
+      /* Loop over incoming aircraft and add or update if there are changes. */
       for (const icaoId in aircraftStatuses) {
-        const status = aircraftStatuses[icaoId] as AircraftStatusData
-        aircraftMap.addUpdatePlane(status)
+        previousStatus = Optional.ofNullable(previousAircraftStatuses[icaoId])
+        currentStatus = Optional.ofNullable(aircraftStatuses[icaoId])
+
+        if (previousStatus.isPresent()) {
+          const previousString = JSON.stringify(previousStatus.get())
+          const currentString = JSON.stringify(currentStatus.get())
+          if (previousString !== currentString) {
+            aircraftMap.addUpdatePlane(currentStatus.get())
+          }
+        } else {
+          aircraftMap.addUpdatePlane(currentStatus.get())
+        }
       }
+
+      for (let icaoId in previousAircraftStatuses) {
+        currentStatus = Optional.ofNullable(aircraftStatuses[icaoId])
+        if (!currentStatus.isPresent()) {
+          aircraftMap.removePlane(icaoId)
+        }
+      }
+
+      // clone the current aircraft statuses so we can compare them next time
+      previousAircraftStatuses = { ...aircraftStatuses }
+
+      // /* Remove planes that are no longer in the store. Could we do this with
+      //    the same loop the removed them from the store? Can that loop access
+      //    the map? */
+      // for (const plane of aircraftMap.fetchPlanes()) {
+      //   const status = aircraftStatuses[plane.icaoId]
+      //   if (!status) aircraftMap.removePlane(plane.icaoId)
+      // }
+
+      // /* Add or update planes that *are* in the store. */
+      // for (const icaoId in aircraftStatuses) {
+      //   const status = aircraftStatuses[icaoId] as AircraftStatusData
+      //   aircraftMap.addUpdatePlane(status)
+      // }
     })
 
     return {
